bebb36fa9a90baeb094a7bd244fc213699724c04
Farhan <71598324+FarhanNoor02@users.noreply.github.com>
2026-06-26 13:26:01 +0600

    Update help_manager.py
    BZ and XRD documentation

--- utils/help_manager.py
+++ utils/help_manager.py
@@ -2,10 +2,10 @@ import sys
 
 def display_help():
     """Prints a professional, categorized help menu to the terminal."""
-    help_text = """
+    help_text = r"""
 QE-Kit: A Pre- & Post-Processing Suite for Quantum ESPRESSO
 ===========================================================
-Version: 0.3.3 | Lead Dev: Farhan Noor
+Version: 0.3.4 | Lead Dev: Farhan Noor (Univ. of Dhaka)
 
 USAGE:
     qekit           : Launch interactive menu (Default)
@@ -14,22 +14,28 @@ USAGE:
 
 ZONE 1: STRUCTURAL DISCOVERY
     --100 : Convert .cif files to PWscf input templates.
-    --101 : Interactive Pseudopotential selection. Selects from the QE-POTCAR library which is to
-            be installed during initial setup of qekit
-    --102 : Generate K-point grid. 
-            Available options: A) Uniform (Automatic grid),(B) Homogeneous (Explicit crystal grid;
-            list all Kpoints- for example: used for optical spectra), (C) Band (High-symmetry path)
+    --101 : Interactive Pseudopotential selection. Selects from the QE-POTCAR 
+            library, which must be configured during initial setup.
+    --102 : Generate K-point grid. Available options: 
+            (A) Uniform (Automatic grid)
+            (B) Homogeneous (Explicit crystal grid; e.g., for optical spectra)
+            (C) Band (High-symmetry path)
 
 ZONE 2: INPUT ARCHITECT
-    --200 : Generate SCF input files. Users will specify the ecut and ewfc thresholds; to be read
-            from the pseudopotential files
+    --200 : Generate SCF input files. Users will specify the ecutwfc and ecutrho 
+            thresholds, read directly from the pseudopotential files.
     --206 : Generate Phonon (ph.in) input files (Automated).
 
 ZONE 3: PHYSICS & ANALYSIS
     --300 : Symmetry refinement (Updates scf.in with optimized lattice).
-    --301 : Phonon post-processing (Automated). Generates files for use with q2r.x, matdyn.x and
-            plotband.x
+    --301 : Phonon post-processing (Automated). Generates files for use with 
+            q2r.x, matdyn.x, and plotband.x.
     --303 : Isotropic optical constant derivation (n, k, R, alpha).
+
+ZONE 4: WORKFLOW & VISUALIZATION
+    --401 : Run HT-Phonon Pipeline (Bash Driver).
+    --404 : Interactive Brillouin Zone Visualizer (Matplotlib).
+    --405 : Calculated XRD Pattern Plotter (Pymatgen).
 
 DOCUMENTATION & SUPPORT:
     GitHub: https://github.com/FarhanNoor02/QE-Kit
